7088d209733bd542085f4820ce9a00d878393f7e
puneetjain91 <40116199+puneetjain91@users.noreply.github.com>
2021-01-15 22:16:52 +0530

    Update test.spec.js

--- src/__test__/test.spec.js
+++ src/__test__/test.spec.js
@@ -3,3 +3,13 @@ describe("Testing our nodeJS app", () => {
     expect(2).toBe(9);
   });
 });
+describe("Testing our nodeJS app", () => {
+  it("Testing using Github Actions", () => {
+    expect(2).toBe(2);
+  });
+});
+describe("Testing our nodeJS app", () => {
+  it("Testing using Github Actions", () => {
+    expect(3).toBe(3);
+  });
+});
