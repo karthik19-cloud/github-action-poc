303ad09d87907adc8ae570ac7db6a357a02ca1ac
nilachak <47153541+nilachak@users.noreply.github.com>
2021-01-15 19:30:01 +0530

    Update test.spec.js

--- src/__test__/test.spec.js
+++ src/__test__/test.spec.js
@@ -3,3 +3,15 @@ describe("Testing our nodeJS app", () => {
     expect(2).toBe(2);
   });
 });
+
+describe('Google Search Test', () => {
+
+    it('should have the right title', (done) => {
+      browser.url('https://google.com/ncr')
+      search_box_element = $('[name=\'q\']')
+      search_box_element.setValue("BrowserStack\n")
+
+      expect(browser).toHaveTitle("BrowserStack - Google Search");
+    })
+
+})
