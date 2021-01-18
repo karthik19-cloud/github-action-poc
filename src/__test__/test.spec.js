1a7e4b21448c4e21fe1f3780472176fe2a4b6063
karthik19-cloud <32713545+karthik19-cloud@users.noreply.github.com>
2021-01-18 17:09:36 +0530

    Update test.spec.js

--- src/__test__/test.spec.js
+++ src/__test__/test.spec.js
@@ -1,11 +1,21 @@
+import sum from './sum.js';
+ 
+describe('sum function', () => {
+  it('sums up two integers', () => {
+    expect(sum(1, 2)).toEqual(3);
+  });
+});
+
+describe('sum function', () => {
+  it('sums up two integers', () => {
+    expect(sum(2, 3)).toEqual(5);
+  });
+});
+
 describe("Testing our nodeJS app", () => {
   it("Testing using Github Actions", () => {
     expect(2).toBe(2);
   });
 });
 
-describe("testing #2 our nodeJS app", () => {
-  it("Testing using Github Actions", () => {
-    expect(2).toBe(2);
-  });
-});
+
