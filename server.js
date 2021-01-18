02038c590e087ba9ba0781c22416c18b2f39381e
karthik19-cloud <32713545+karthik19-cloud@users.noreply.github.com>
2021-01-18 18:04:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,11 +12,5 @@ app.get('/', (req, res) => {
   res.send('GitHub Action World!');
 });
 
-function sum(a, b) {
-  return a + b;
-}
- 
-export default sum;
-
 app.listen(PORT, HOST);
 console.log(`Running hello on http://${HOST}:${PORT}`);
